0e653cb6b2b66a48385e79528f7eb70624554892
Clive C.Banditi <clive@cornelo.us>
2014-11-13 05:32:38 +0200

    Some styling for the custom messages

--- application/views/account/msg.php
+++ application/views/account/msg.php
@@ -6,17 +6,15 @@ window.location.href = "http://cornelo.us/index.php"; //will redirect to your bl
 
 <table id="success">
     <tr>
-        <th>Msg</th>
+        <th>Success</th>
         <th></th>
     </tr>
 
     <tr>
+        <?php if ($msg == 'reset'): ?>
         <td>
-
+            <?php echo 'Your password has been successfully reset, please check your email' ?>
         </td>
-    </tr>
-    <tr>
-        <td>
-        </td>
+        <?php endif ?>
     </tr>
 </table>
