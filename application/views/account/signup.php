c50651f4df4b65d05b12992c232d9c445a061b36
Clive C.Banditi <clive@cornelo.us>
2014-11-13 03:41:50 +0200

    Validations for sign up form

--- application/views/account/signup.php
+++ application/views/account/signup.php
@@ -120,8 +120,7 @@
     <tr>
         <td>
             <?php echo Form::label('country', 'Country')?>:<br>
-            <?php echo Form::input('country', $country); ?>
-            <?php echo Form::select('country', array('1'=>'ANGOLA','2'=>'MALAWI','3'=>'SOUTH AFRICA', '4'=>'ZIMBABWE',)); ?>
+            <?php echo Form::select('country', array('0' =>'', '1'=>'ANGOLA','2'=>'MALAWI','3'=>'SOUTH AFRICA', '4'=>'ZIMBABWE',)); ?>
         </td>
         <td>
             <?php if ($errors): ?>
