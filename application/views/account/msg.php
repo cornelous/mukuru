164e4b6e034821656c87f298693e0cf7b3ab6051
Clive C.Banditi <clive@cornelo.us>
2014-11-13 04:48:23 +0200

    Javascript time delay

--- application/views/account/msg.php
+++ application/views/account/msg.php
@@ -1,3 +1,9 @@
+<script>
+setTimeout(function () {
+window.location.href = "http://cornelo.us/index.php"; //will redirect to your blog page (an ex: blog.html)
+}, 5000); //will call the function after 2 secs.
+</script>
+
 <table id="login">
     <tr>
         <th>Msg</th>
