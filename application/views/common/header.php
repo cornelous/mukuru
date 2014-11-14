defa34e53a7da436e8e98df609e476b8e23e21f4
Clive C.Banditi <clive@cornelo.us>
2014-11-14 13:58:55 +0200

    Edit user details

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -8,8 +8,8 @@
     $user = $session->get('username');
     if ($user) : ?>
         You are logged in as: <?php echo $user; ?>.
+        <?php echo HTML::anchor("edit?uname={$user}", 'Update Details'); ?> |
         <?php echo HTML::anchor('logout', 'Logout');?>
-        <?php echo HTML::anchor("edit?uname={$user}", 'Update Details'); ?>
     <?php else: ?>
             <?php echo HTML::anchor('signup', 'Sign Up');?> | <?php echo HTML::anchor('login', 'Login');?> | <?php  echo HTML::anchor('reset', 'Reset password');?>
     <?php endif; ?>
