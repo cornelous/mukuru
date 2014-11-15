830c2ace5bfc4b581debc1629ac3d3cf2c2a6d95
Clive C.Banditi <clive@cornelo.us>
2014-11-15 04:26:15 +0200

    Country select updates to views

--- application/classes/controller/user/account.php
+++ application/classes/controller/user/account.php
@@ -297,6 +297,7 @@ class Controller_User_Account extends Controller_Application {
             $status = $_GET['status'];
             $activ = new Model_User;
             $result = $activ->activ($uid, $status);
+            var_dump($result);
             if ($result){
                 $this->request->redirect($referrer);
             }
