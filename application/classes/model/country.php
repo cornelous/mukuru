c4bf27c64338281d83e167a4fa340a58c03a7365
Clive C.Banditi <clive@cornelo.us>
2014-11-15 03:38:27 +0200

    Fixing country select

--- application/classes/model/country.php
+++ application/classes/model/country.php
@@ -8,6 +8,6 @@ class Model_Country {
         return DB::select()
             ->from($this->_table)
             ->execute()
-            ->as_array('name', 'name');
+            ->as_array('id', 'country');
     }
 }
